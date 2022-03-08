22d080c2f171a8af2005dc0d978139812824e95c
Iliass <iliasselyaa@hotmail.com>
2022-03-08 17:46:36 +0000

    centred app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import WeatherWidget from "./components/WeatherWidget";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h3>ff</h3>
-      <h2>huhohjk</h2>
-      <h1>sadasdas</h1>
+      <centre>
+        <header>
+          <h1>Test</h1>
+          <WeatherWidget />
+        </header>
+      </centre>
     </div>
   );
 }
